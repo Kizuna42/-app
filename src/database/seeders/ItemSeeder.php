11909b015b8fd1ae273aa0fa9bcb22e1e47f850e
Kizuna Ishino <131520471+Kizuna42@users.noreply.github.com>
2025-01-08 01:22:11 +0900

    picture

--- src/database/seeders/ItemSeeder.php
+++ src/database/seeders/ItemSeeder.php
@@ -15,7 +15,7 @@ class ItemSeeder extends Seeder
     public function run(): void
     {
         $user = User::first() ?? User::factory()->create();
-        
+
         // カテゴリーIDを取得
         $fashionId = Category::where('name', 'ファッション')->first()->id;
         $electronicsId = Category::where('name', '家電')->first()->id;
@@ -27,7 +27,7 @@ class ItemSeeder extends Seeder
                 'name' => '腕時計',
                 'price' => 15000,
                 'description' => 'スタイリッシュなデザインのメンズ腕時計',
-                'image' => 'items/Armani+Mens+Clock.jpg',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Armani+Mens+Clock.jpg',
                 'condition' => 'good',
                 'category_id' => $fashionId,
             ],
@@ -35,7 +35,7 @@ class ItemSeeder extends Seeder
                 'name' => 'HDD',
                 'price' => 5000,
                 'description' => '高速で信頼性の高いハードディスク',
-                'image' => 'items/HDD+Hard+Disk.jpg',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/HDD+Hard+Disk.jpg',
                 'condition' => 'good',
                 'category_id' => $electronicsId,
             ],
@@ -43,7 +43,7 @@ class ItemSeeder extends Seeder
                 'name' => '玉ねぎ3束',
                 'price' => 300,
                 'description' => '新鮮な玉ねぎ3束のセット',
-                'image' => 'items/iLoveIMG+d.jpg',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/iLoveIMG+d.jpg',
                 'condition' => 'fair',
                 'category_id' => $otherId,
             ],
@@ -51,7 +51,7 @@ class ItemSeeder extends Seeder
                 'name' => '革靴',
                 'price' => 4000,
                 'description' => 'クラシックなデザインの革靴',
-                'image' => 'items/Leather+Shoes+Product+Photo.jpg',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Leather+Shoes+Product+Photo.jpg',
                 'condition' => 'poor',
                 'category_id' => $fashionId,
             ],
@@ -59,7 +59,7 @@ class ItemSeeder extends Seeder
                 'name' => 'ノートPC',
                 'price' => 45000,
                 'description' => '高性能なノートパソコン',
-                'image' => 'items/Living+Room+Laptop.jpg',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Living+Room+Laptop.jpg',
                 'condition' => 'good',
                 'category_id' => $electronicsId,
             ],
@@ -67,7 +67,7 @@ class ItemSeeder extends Seeder
                 'name' => 'マイク',
                 'price' => 8000,
                 'description' => '高音質のレコーディング用マイク',
-                'image' => 'items/Music+Mic+4632231.jpg',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Music+Mic+4632231.jpg',
                 'condition' => 'good',
                 'category_id' => $electronicsId,
             ],
@@ -75,7 +75,7 @@ class ItemSeeder extends Seeder
                 'name' => 'ショルダーバッグ',
                 'price' => 3500,
                 'description' => 'おしゃれなショルダーバッグ',
-                'image' => 'items/Purse+fashion+pocket.jpg',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Purse+fashion+pocket.jpg',
                 'condition' => 'fair',
                 'category_id' => $fashionId,
             ],
@@ -83,7 +83,7 @@ class ItemSeeder extends Seeder
                 'name' => 'タンブラー',
                 'price' => 500,
                 'description' => '使いやすいタンブラー',
-                'image' => 'items/Tumbler+souvenir.jpg',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Tumbler+souvenir.jpg',
                 'condition' => 'poor',
                 'category_id' => $interiorId,
             ],
@@ -91,7 +91,7 @@ class ItemSeeder extends Seeder
                 'name' => 'コーヒーミル',
                 'price' => 4000,
                 'description' => '手動のコーヒーミル',
-                'image' => 'items/Waitress+with+Coffee+Grinder.jpg',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Waitress+with+Coffee+Grinder.jpg',
                 'condition' => 'good',
                 'category_id' => $interiorId,
             ],
@@ -99,7 +99,7 @@ class ItemSeeder extends Seeder
                 'name' => 'メイクセット',
                 'price' => 2500,
                 'description' => '便利なメイクアップセット',
-                'image' => 'items/外出メイクアップセット.jpg',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/%E5%A4%96%E5%87%BA%E3%83%A1%E3%82%A4%E3%82%AF%E3%82%A2%E3%83%83%E3%83%95%E3%82%9A%E3%82%BB%E3%83%83%E3%83%88.jpg',
                 'condition' => 'good',
                 'category_id' => $otherId,
             ],
@@ -118,4 +118,4 @@ class ItemSeeder extends Seeder
             ]);
         }
     }
-} 
+}
--- src/resources/views/items/index.blade.php
+++ src/resources/views/items/index.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1 class="mb-4">商品一覧</h1>
+<div class="container mt-5">
+    <ul class="nav nav-tabs justify-content-center mb-4">
+        <li class="nav-item">
+            <a class="nav-link {{ request('tab') !== 'mylist' ? 'active' : '' }}" href="{{ route('items.index', ['tab' => 'recommend']) }}">おすすめ</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ request('tab') === 'mylist' ? 'active' : '' }}" href="{{ route('items.index', ['tab' => 'mylist']) }}">マイリスト</a>
+        </li>
+    </ul>
+
     <div class="row">
         @foreach($items as $item)
-            <div class="col-md-4 mb-4">
+            <div class="col-md-3 mb-4">
                 <div class="card">
-                    @if($item->image)
-                        <img src="{{ asset('storage/' . $item->image) }}" class="card-img-top" alt="{{ $item->name }}">
-                    @endif
+                    <img src="{{ $item->image }}" class="card-img-top" alt="{{ $item->name }}">
                     <div class="card-body">
                         <h5 class="card-title">{{ $item->name }}</h5>
-                        <p class="card-text">¥{{ number_format($item->price) }}</p>
-                        <a href="{{ route('items.show', $item->id) }}" class="btn btn-primary">詳細を見る</a>
                     </div>
                 </div>
             </div>
         @endforeach
     </div>
 </div>
-@endsection 
+@endsection
