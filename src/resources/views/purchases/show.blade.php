cc0f946f628c426b0c1ca2c9623bab0f933a1ae5
Kizuna Ishino <131520471+Kizuna42@users.noreply.github.com>
2025-01-15 19:34:34 +0900

    purchases.show

--- src/resources/views/purchases/show.blade.php
+++ src/resources/views/purchases/show.blade.php
@@ -1,58 +1,89 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1 class="mb-4">購入確認</h1>
-    <div class="row">
+<div class="container-fluid px-5 mt-4">
+    <div class="row gx-5">
+        <!-- 左側：商品情報 -->
         <div class="col-md-8">
-            <div class="card mb-4">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-4">
-                            @if($item->image)
-                                <img src="{{ asset('storage/' . $item->image) }}" class="img-fluid" alt="{{ $item->name }}">
-                            @endif
-                        </div>
-                        <div class="col-md-8">
-                            <h5 class="card-title">{{ $item->name }}</h5>
-                            <p class="card-text">¥{{ number_format($item->price) }}</p>
-                        </div>
+            <div class="mb-5 pb-4 border-bottom">
+                <div class="row align-items-center">
+                    <div class="col-md-5">
+                        @if($item->image)
+                            <img src="{{ $item->image }}" class="img-fluid" alt="{{ $item->name }}">
+                        @endif
+                    </div>
+                    <div class="col-md-7">
+                        <h3 class="mb-3">{{ $item->name }}</h3>
+                        <h4 class="mb-0">¥{{ number_format($item->price) }}</h4>
                     </div>
                 </div>
             </div>
 
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title mb-4">配送先住所</h5>
-                    <form action="{{ route('purchases.store', $item->id) }}" method="POST">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="postal_code" class="form-label">郵便番号</label>
-                            <input type="text" class="form-control @error('postal_code') is-invalid @enderror" id="postal_code" name="postal_code" required>
-                            @error('postal_code')
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="address" class="form-label">住所</label>
-                            <input type="text" class="form-control @error('address') is-invalid @enderror" id="address" name="address" required>
-                            @error('address')
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <button type="submit" class="btn btn-primary">購入を確定する</button>
-                    </form>
+            <!-- 支払い方法 -->
+            <div class="mb-5 pb-4 border-bottom">
+                <h4 class="mb-4">支払い方法</h4>
+                <select class="form-select form-select-lg border-0 ps-0 @error('payment_method') is-invalid @enderror" name="payment_method" required>
+                    <option value="" selected disabled>選択してください</option>
+                    <option value="convenience">コンビニ払い</option>
+                    <option value="credit">カード支払い</option>
+                </select>
+                @error('payment_method')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <!-- 配送先 -->
+            <div class="mb-5 pb-4 border-bottom">
+                <div class="d-flex justify-content-between align-items-center mb-4">
+                    <h4 class="mb-0">配送先</h4>
+                    <a href="{{ route('purchases.address.edit', $item) }}" class="text-primary text-decoration-none">変更する</a>
                 </div>
+                @if(auth()->user()->postal_code && auth()->user()->address)
+                    <p class="mb-2 fs-5">〒{{ substr(auth()->user()->postal_code, 0, 3) }}-{{ substr(auth()->user()->postal_code, 3) }}</p>
+                    <p class="mb-0 fs-5">{{ auth()->user()->address }}</p>
+                @else
+                    <p class="text-muted mb-0 fs-5">配送先住所を登録してください</p>
+                @endif
             </div>
         </div>
+
+        <!-- 右側：購入確認 -->
         <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">購入金額</h5>
-                    <p class="h3">¥{{ number_format($item->price) }}</p>
+            <div class="position-sticky" style="top: 2rem;">
+                <!-- 金額・支払い方法の確認 -->
+                <div class="border rounded mb-4">
+                    <div class="border-bottom p-4">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span class="fs-5">商品代金</span>
+                            <span class="h4 mb-0">¥{{ number_format($item->price) }}</span>
+                        </div>
+                    </div>
+                    <div class="p-4">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span class="fs-5">支払い方法</span>
+                            <span id="selected-payment" class="text-muted fs-5">選択してください</span>
+                        </div>
+                    </div>
                 </div>
+
+                <!-- 購入ボタン -->
+                <form action="{{ route('purchases.store', $item) }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="payment_method" id="payment_method_hidden">
+                    <button type="submit" class="btn btn-danger btn-lg w-100 py-3 rounded-3">購入する</button>
+                </form>
             </div>
         </div>
     </div>
 </div>
-@endsection 
+
+@push('scripts')
+<script>
+document.querySelector('select[name="payment_method"]').addEventListener('change', function() {
+    const selectedText = this.options[this.selectedIndex].text;
+    document.getElementById('selected-payment').textContent = selectedText;
+    document.getElementById('payment_method_hidden').value = this.value;
+});
+</script>
+@endpush
+@endsection
