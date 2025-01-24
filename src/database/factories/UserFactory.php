aa1807f4e97ba7353b7ee2286c066a783e899c3c
Kizuna Ishino <131520471+Kizuna42@users.noreply.github.com>
2025-01-25 00:06:10 +0900

    token

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -2,11 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
+    protected $model = User::class;
+
     /**
      * Define the model's default state.
      *
@@ -15,11 +18,14 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => bcrypt('password'), // パスワードのハッシュ化
+            'avatar' => $this->faker->imageUrl(),
+            'postal_code' => $this->faker->postcode(),
+            'address' => $this->faker->address(),
+            'building_name' => $this->faker->optional()->streetAddress, // buildingNameを適切なフォーマットに変更
         ];
     }
 
--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->string('postal_code', 7)->nullable();
             $table->string('address')->nullable();
             $table->string('building_name')->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
